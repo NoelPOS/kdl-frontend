071eec9108a39ca0c7aa713827d8b5b55e7919a0
Noel Paing Oak Soe <159676867+NoelPOS@users.noreply.github.com>
2025-09-25 23:58:10 +0700

    fix: hide statistics

--- components/layout/sidebar/sidebar-detail.tsx
+++ components/layout/sidebar/sidebar-detail.tsx
@@ -92,12 +92,12 @@ const menuItems: MenuItem[] = [
     url: "/management-fee",
     roles: [UserRole.ADMIN],
   },
-  {
-    title: "statistics",
-    icon: BarChart3,
-    url: "/statistics",
-    roles: [UserRole.ADMIN],
-  },
+  // {
+  //   title: "statistics",
+  //   icon: BarChart3,
+  //   url: "/statistics",
+  //   roles: [UserRole.ADMIN],
+  // },
   // {
   //   title: "notifications",
   //   icon: Bell,
